f654d36ffa0056920bc3541ececd3f1eb7eb3c19
zulang21 <arthurricken1@gmail.com>
2025-05-04 23:40:59 -0300

    traduçao fica salva conforme a ultima deixada

--- JS/index.js
+++ JS/index.js
@@ -4,27 +4,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const temaLocal = localStorage.getItem("tema");
     document.body.setAttribute("data-theme", temaLocal);
+
+    const idiomaSalvo = localStorage.getItem("idioma") ;
+    idiomaAtual = idiomaSalvo;
+
+    const traducaoCache = localStorage.getItem(`traducao_${idiomaAtual}`);
+    if (traducaoCache) {
+        traduzirPagina(JSON.parse(traducaoCache));
+    } else {
+        carregaIdioma(idiomaAtual);
+    }
 });
 
 let idiomaAtual = "pt"
 
 function alterarIdioma() {
     idiomaAtual = idiomaAtual == "pt" ? "en" : "pt";
+    localStorage.setItem("idioma", idiomaAtual);
     carregaIdioma(idiomaAtual);
 
 }
 
 function carregaIdioma(idioma){
-    fetch(`./JSON/${idioma}.json`).then(data => data.json()).then(data=>{
+    fetch(`./JSON/${idioma}.json`)
+    .then(data => data.json())
+    .then(data=>{
+        localStorage.setItem(`traducao_${idioma}`, JSON.stringify(data));
         traduzirPagina(data);
     });
 }
 
 function traduzirPagina(linguagem){
-    document.querySelectorAll("[data-i18n]").forEach(elemento=>{
-        console.log(elemento);
+    document.querySelectorAll("[data-i18n]").forEach(elemento=>{ 
         const chave = elemento.getAttribute("data-i18n");
-        console.log(chave);
         if(linguagem[chave]){
             elemento.textContent = linguagem[chave];
         }
